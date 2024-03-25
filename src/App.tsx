dc5a3d76e9f3fc0ebae5eff5dec12089a75e2023
JeremiPam <jeremipamula@gmail.com>
2024-03-25 12:29:00 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button>asdas</Button>
+    </>
+  );
+}
+
+export default App;
